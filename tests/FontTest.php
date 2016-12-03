3989fca6bff091d125a33c1b413c826f0bd6e825
Pierre Rudloff <contact@rudloff.pro>
2016-12-03 11:40:52 +0100

    Add test for getBowerJson()

--- tests/FontTest.php
+++ tests/FontTest.php
@@ -42,4 +42,31 @@ class FontTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertEquals('aclonica-googlefont', $this->font->getBowerName());
     }
+
+    /**
+     * Test getBowerJson function.
+     *
+     * @return void
+     */
+    public function testGetBowerJson()
+    {
+        $this->assertEquals('{
+    "name": "aclonica-googlefont",
+    "license": "APACHE2",
+    "authors": [
+        "Astigmatic"
+    ],
+    "homepage": "https://fonts.google.com/",
+    "repository": {
+        "type": "git",
+        "url": "https://github.com/google-fonts-bower/aclonica-bower.git"
+    },
+    "description": "Aclonica font",
+    "keywords": [
+        "font",
+        "SANS_SERIF"
+    ]
+}
+', $this->font->getBowerJson());
+    }
 }
