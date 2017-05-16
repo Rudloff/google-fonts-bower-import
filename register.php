84a9b95d1280cb21f86afb64b5ca2c1151f7efa5
Pierre Rudloff <contact@rudloff.pro>
2017-05-16 23:39:30 +0200

    Use local Bower Move Grunt to dev dependencies

--- register.php
+++ register.php
@@ -7,6 +7,6 @@ $importer = new GoogleFontsBower\Importer();
 foreach ($importer->getFonts() as $font) {
     echo $importer->log($font, 'Registering '.$font->getBowerName().'…');
     exec(
-        'bower register '.$font->getBowerName().' '.$importer->getFontRepoUrl($font)
+        './node_modules/.bin/bower register '.$font->getBowerName().' '.$importer->getFontRepoUrl($font)
     );
 }
